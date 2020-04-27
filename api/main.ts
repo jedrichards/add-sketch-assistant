6b974fed3244df60a70f73c680b14286775ab988
jedrichards <jedrichards@gmail.com>
2020-04-27 14:24:26 +0100

    Test

--- api/main.ts
+++ api/main.ts
@@ -1,7 +1,17 @@
 import { NowRequest, NowResponse } from '@now/node'
+import fetch from 'node-fetch'
+import getNpmTarballUrl from 'get-npm-tarball-url'
 
-function assertUrl(val: unknown): asserts val is string {
-  if (typeof val !== 'string') throw Error('url query value invalid')
+function assertPkg(val: unknown): asserts val is string {
+  if (typeof val !== 'string') throw Error('pkg is invalid')
+}
+
+function assertTag(val: unknown): asserts val is string {
+  if (typeof val !== 'string') throw Error('tag is invalid')
+}
+
+function assertSemver(val: unknown): asserts val is string {
+  if (typeof val !== 'string') throw Error('no valid pkg semver found')
 }
 
 function assertVariant(val: unknown): asserts val is string {
@@ -10,15 +20,25 @@ function assertVariant(val: unknown): asserts val is string {
   }
 }
 
-export default (request: NowRequest, response: NowResponse) => {
+export default async (request: NowRequest, response: NowResponse) => {
   try {
-    const { variant = '', url } = request.query
+    const { variant = '', tag = 'latest', pkg } = request.query
     assertVariant(variant)
-    assertUrl(url)
+    assertPkg(pkg)
+    assertTag(tag)
+    // Use the npm API to get the latest semver for the given dist tag
+    const res = await fetch(`https://registry.npmjs.org/${pkg}`, {
+      headers: { Accept: 'application/vnd.npm.install-v1+json' },
+    })
+    const data = await res.json()
+    const semver = data['dist-tags'][tag]
+    assertSemver(semver)
+    // Redirect to the one-click install url
+    const protocol = variant === '' ? 'sketch' : `sketch-${variant}`
     response.status(307)
     response.setHeader(
       'Location',
-      `sketch${variant ? `-${variant}` : ''}://install-assistant?url=${url}`,
+      `${protocol}}://install-assistant?url=${getNpmTarballUrl(pkg, semver)}`,
     )
     response.send('')
   } catch (error) {
